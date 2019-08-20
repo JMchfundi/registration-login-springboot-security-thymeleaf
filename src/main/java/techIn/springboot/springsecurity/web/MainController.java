31cc58d12124083f026aa15e6e80b4b5e8fea2e4
JMchfundi <akidamjaffar@gmail.com>
2019-08-20 15:36:53 -0400

    https://github.com/JMchfundi/techIn.git

--- src/main/java/techIn/springboot/springsecurity/web/MainController.java
+++ src/main/java/techIn/springboot/springsecurity/web/MainController.java
@@ -1,11 +1,23 @@
 package techIn.springboot.springsecurity.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import techIn.springboot.springsecurity.model.User;
+import techIn.springboot.springsecurity.repository.UserRepository;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/")
     public String root() {
@@ -20,5 +32,15 @@ public class MainController {
     @GetMapping("/user")
     public String userIndex() {
         return "user/index";
+    }
+
+    @RequestMapping(value = "/viewusers")
+    public  String viewusers (Model model) {
+
+        List<User> users = userRepository.findAll();
+
+        model.addAttribute("users", users);
+
+        return "viewusers";
     }
 }
